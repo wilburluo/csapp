1f477163507cc5565ed45fb6a49fbcd0c96339fd
wilburluo@legion <wilburluo@outlook.com>
2024-06-05 10:45:17 +0800

    ch02.

---
+++ ch02_representing_and_manupulating_information/02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <limits.h>
+#include "show_bytes.h"
+
+int main(void)
+{
+    show_int(12345);
+    show_float(12345.0f); 
+
+    int x = -1;
+    unsigned u = 2147483648;
+
+    printf("x = %u = %d\n", x, x);
+    printf("u = %u = %d\n", u, u);
+    printf("%d\n", u - UINT32_MAX);
+    int i1 = (-1 < 0U)?1:0; 
+    int i2 = (-1 < 0)?1:0; 
+    printf("%d\t%d\n", i1,i2);
+    return 0;
+}
