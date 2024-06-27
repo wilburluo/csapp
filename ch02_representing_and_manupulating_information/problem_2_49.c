1fc51fe89ba5e6138ea5c7cef1c5f9acdafdf4f1
rowe@gentomen <wilburluo@outlook.com>
2024-06-27 22:57:23 +0800

    solution of problem 2.49.

--- ch02_representing_and_manupulating_information/problem_2_49.c
+++ ch02_representing_and_manupulating_information/problem_2_49.c
@@ -1,3 +1,13 @@
 //
 // Created by rowe on 24-6-27.
 //
+#include <stdio.h>
+
+int main(void) {
+    float f1 = 0x1p24; //2^24次方表示方法，用p或P
+    float f2 = 0xap2;
+    printf("0x1p24: %f\n", f1);
+    printf("0x1p24 + 1: %f\n", f1 + 1.0f);
+    printf("0xap2: %f\n", f2);
+    return 0;
+}
